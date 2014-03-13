6347abfaa68e3c604939f692a41cef2b17f724bf
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2014-03-13 13:07:26 +0100

    update tools abstraction

---
+++ Wakanda-Analytics-Proxy/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,18 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		document.location = 'https://github.com/AMorgaut/Wakanda-Analytics-Proxy';
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
